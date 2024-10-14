7c78398f15b3e25c6cb16ca1afe6746a791a8a26
Paul Deitel <paul@deitel.com>
2024-10-14 09:51:40 -0400

    Update fig06_02.cpp
    fixed `values2` in last `for` loop

--- examples/ch06/fig06_02.cpp
+++ examples/ch06/fig06_02.cpp
@@ -18,8 +18,8 @@ int main() {
    std::array values2{1.1, 2.2, 3.3, 4.4};
 
    // output each array element's value
-   for (size_t i{0}; i < values.size(); ++i) {
-      std::cout << fmt::format("{}  ", values.at(i));
+   for (size_t i{0}; i < values2.size(); ++i) {
+      std::cout << fmt::format("{}  ", values2.at(i));
    }
 
    std::cout << "\n";
